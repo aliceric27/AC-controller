0a67d3fad4d94eed83883c4603794811de0c5407
wei <yuki787817@dayeasy-ai.com>
2023-10-02 15:52:19 +0800

    project init

---
+++ pages/header.vue
@@ -0,0 +1,12 @@
+<template>
+    <header class="border grid grid-cols-3">
+        <div class="border">one</div>
+        <div class="border">two</div>
+        <div class="border">three</div>
+    </header>
+</template>
+<script setup>
+</script>
+
+<style scoped>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1 class="text-red-500">測試</h1>
+</template>
