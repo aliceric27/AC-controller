d73a05f67a15a2d4d6171b6000b4327e85654e32
wei <yuki787817@dayeasy-ai.com>
2023-10-04 16:33:49 +0800

    新增樓層提示

---
+++ components/RoomSelect.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="sticky flex justify-center w-full top-14">
+    <select
+      class="max-w-[17.5rem] w-full p-2 my-2 bg-[#cccccc] rounded-lg text-2xl font-bold bg-opacity-90"
+      v-model="selectedroom"
+      @change="handleChange"
+    >
+      <option value="1">{{ $t("pickroom") }}</option>
+      <option v-for="val in roomlist()" :value="val">
+        {{ `${val}房` }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const emit = defineEmits(["change"]);
+const handleChange = (event: Event) => {
+  const value = (event.target as HTMLSelectElement).value;
+  emit("change", Number(value)); // assuming the value is a number
+};
+// tmp fake data
+const selectedroom = ref(1);
+const roomlist = () => {
+  let result = [];
+  for (let i = 0; i < 30; i++) {
+    result.push(i);
+  }
+  return result;
+};
+</script>
+
+<style scoped></style>
--- pages/room-page/[id].vue
+++ pages/room-page/[id].vue
@@ -1,8 +1,46 @@
 <template>
-  <FloorSelect />
-  <div>測試 {{ $route.params.id }}</div>
+  <div class="warp">
+    <Theheader />
+    <div class="inline-flex items-center justify-center w-full gold-text">
+      {{ $route.params.id }}樓
+    </div>
+    <Floorlist />
+    <RoomSelect @change="movetoid" />
+    <AirCard :id="`id-${val}`" v-for="val in fakeroom()" />
+  </div>
 </template>
 <script setup lang="ts">
 import { useRoute } from "vue-router";
 const route = useRoute();
+// tmp fake data
+const fakeroom = () => {
+  let result = [];
+  for (let i = 0; i < 30; i++) {
+    result.push(i);
+  }
+  return result;
+};
+const movetoid = (event: Event) => {
+  const element = event.target as HTMLElement; // 提取 target 属性
+  console.log(element);
+  if (element) {
+    element.scrollIntoView({ behavior: "smooth" });
+  } else {
+    console.error("Element not found:", element);
+  }
+};
 </script>
+<style scoped>
+.warp {
+  background: url(~/image-10.png);
+}
+.gold-text {
+  color: #c2a344;
+  font-family: Microsoft JhengHei UI;
+  font-size: 2.5rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  letter-spacing: 0.25rem;
+}
+</style>
