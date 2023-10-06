5086f4faf81fcec211f393a5da5b232957ebf7a7
wei <yuki787817@dayeasy-ai.com>
2023-10-06 13:37:15 +0800

    將首頁改用layout寫法

---
+++ layouts/default.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="warp">
+    <!-- Header -->
+    <Theheader />
+    <!-- 樓層卡片資料 -->
+    <div class="flex flex-col items-center">
+      <Floorlist />
+      <div v-for="val in floorlist">
+        <MainCard :floor="val" @click="turnfloor(val)" />
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import useInfoStore from "~/store/index";
+import { useRoute, useRouter } from "vue-router";
+const route = useRoute();
+const router = useRouter();
+const store = useInfoStore();
+const floorlist = store.getfloor;
+// 樓層跳轉
+const turnfloor = (floor: number) => {
+  store.changeSelectedfloor(floor);
+  store.turnfloor(floor, router);
+};
+</script>
+<style scoped>
+body {
+  height: 100vh;
+  width: 100%;
+}
+.warp {
+  background: url(/image-10.png);
+}
+</style>
