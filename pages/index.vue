8b06919873fcbfe98ef21aa9d7a7949bc28b43b3
wei <yuki787817@dayeasy-ai.com>
2023-10-12 11:39:45 +0800

    interface init

--- pages/index.vue
+++ pages/index.vue
@@ -2,22 +2,22 @@
   <div class="warp">
     <div class="flex flex-col items-center">
       <Floorlist />
-      <div v-for="val in floorlist">
-        <MainCard :floor="val" @click="turnfloor(val)" />
+      <div v-for="val in floordata">
+        <MainCard :floor="val.floorNo" @click="turnfloor(val.floorNo)" />
       </div>
     </div>
   </div>
 </template>
 <script setup lang="ts">
 import useInfoStore from "~/store/index";
-import { useRoute, useRouter } from "vue-router";
+import { useRouter } from "vue-router";
 import { useSocketStore } from "~/store/socketStore";
 const socketStore = useSocketStore();
 socketStore.initializeSocket("http://192.168.0.241:7777");
-const route = useRoute();
 const router = useRouter();
 const store = useInfoStore();
-const floorlist = store.getfloor;
+const floordata = computed(() => socketStore.floorData);
+const floorlist = computed(() => store.getfloor);
 // 樓層跳轉
 const turnfloor = (floor: number) => {
   store.changeSelectedfloor(floor);
--- store/socketStore.ts
+++ store/socketStore.ts
@@ -1,12 +1,52 @@
 import { defineStore, _ActionsTree, _GettersTree } from "pinia";
 import { io, Socket } from "socket.io-client";
+interface Data {
+  rData: {
+    floorData: [];
+    floorList: [];
+  };
+}
+
+interface FloorData {
+  floorNo: number;
+  roomData: {
+    fanSpeed: number;
+    isAuto: number;
+    isWork: number;
+    nowTemp: number;
+    roomNo: String;
+    setMode: number;
+    setTemp: number;
+  }[];
+}
+
+interface FloorList {
+  floorNo: number;
+  offCount: number;
+  offLine: number;
+  onCount: number;
+  roomCount: number;
+  tooCool: number;
+}
+
+export interface State {
+  isConnected: boolean;
+  data: Ref<null | Data>;
+  socket: null | Socket;
+  floorData: Ref<FloorData[] | null>;
+  floorList: Ref<FloorList[] | null>;
+}
+// 初始化資料
+const initState: State = {
+  isConnected: false,
+  data: ref(null),
+  socket: null,
+  floorData: ref([]),
+  floorList: ref([]),
+};
 export const useSocketStore = defineStore({
   id: "socketStore",
-  state: () => ({
-    isConnected: false,
-    data: null,
-    socket: null as Socket | null,
-  }),
+  state: () => initState,
   actions: {
     initializeSocket(endpoint: string) {
       if (!import.meta.env.SSR) {
@@ -14,12 +54,14 @@ export const useSocketStore = defineStore({
           this.socket = io(endpoint);
           this.socket.on("connect", () => {
             this.isConnected = true;
-            console.log("已連接到伺服器");
+            console.log("Connected Socket Server!");
           });
 
-          this.socket.on("tmsList", (data) => {
-            this.data = data;
-            console.log("message", data);
+          this.socket.on("tmsList", (newData) => {
+            this.data = newData;
+            const { floorData, floorList } = newData.rData;
+            this.floorData = floorData;
+            this.floorList = floorList;
           });
 
           this.socket.on("disconnect", () => {
